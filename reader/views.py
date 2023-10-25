5b77807b7109186645545e6fa115f15906b6195c
SamuelFarrel <samuel.farrel@ui.ac.id>
2023-10-26 00:01:09 +0700

    p

--- reader/views.py
+++ reader/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from models import Profile
 
 # Create your views here.
+def get_profile(request):
+    current_profile = Profile.objects.get(request.user)
+    context = {'profile':current_profile}
+    return
+
+def edit_profile(request):
+    current_profile = Profile.objects.get(request.user)
