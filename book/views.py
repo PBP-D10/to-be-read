dbf3c72277fd10be21f1269cc76b13fe92ee64d7
Fredo Melvern Tanzil <fredotanzil@gmail.com>
2023-10-26 20:31:52 +0700

    Implement search

--- book/views.py
+++ book/views.py
@@ -2,11 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
 from book.models import Book
+from django.db.models import Q # untuk chain filter
 
 # Create your views here.
 def home_page(request):
-    books = Book.objects.all().order_by('-date_added')
+    keyword =  request.GET.get("search") or ""
+    if keyword == "":
+        books = Book.objects.all()
+
+    else:
+        books = Book.objects.filter(Q(title__contains=keyword)
+                                          | Q(author__contains=keyword)
+                                          | Q(ISBN__contains=keyword))
+        
+    books = books.order_by('-date_added')
+
     context = {
         'books': books,
+        'keyword': keyword
     }
     return render(request, 'home.html', context=context)
