77e727787fb1d6aa51a0b318e5322332b58c65c7
SamuelFarrel <samuel.farrel@ui.ac.id>
2023-10-26 23:18:53 +0700

    revisi pull request pertama (samuel)

--- reader/urls.py
+++ reader/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'reader'
 
 urlpatterns = [
-    path('myprofile/edit', views.edit_profile, name='edit_profile'),
-    path('myprofile', views.view_profile, name='view_profile'),
+    path('profile/edit', views.edit_profile, name='edit_profile'),
+    path('profile', views.view_profile, name='view_profile'),
 ]
