908659e11e45bdb8b3fc66e18b1c2e410c508c10
SamuelFarrel <samuel.farrel@ui.ac.id>
2023-10-26 15:37:30 +0700

    views function and model, tapi belum nyambung ke html

--- reader/views.py
+++ reader/views.py
@@ -3,9 +3,20 @@ from models import Profile
 
 # Create your views here.
 def get_profile(request):
-    current_profile = Profile.objects.get(request.user)
+    current_profile = Profile.objects.get(user=request.user)
     context = {'profile':current_profile}
     return
 
 def edit_profile(request):
-    current_profile = Profile.objects.get(request.user)
+    profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+
+        if name != profile.name:
+            profile.name = name
+        if email != profile.email:
+            profile.email = email
+        profile.save()
+    return
