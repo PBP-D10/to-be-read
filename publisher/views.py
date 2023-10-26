1829bdd7acde1df5c37554ffd328555c55ba37a7
Joseph BIntang <joseph.bintang@ui.ac.id>
2023-10-26 23:27:44 +0700

    fix publisher

--- publisher/views.py
+++ publisher/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Book
 from .forms import BookForm
+from django.http import JsonResponse
 
 def create_book(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('publisher:book_list')
+            return JsonResponse({'status': 'success'})
+        else:
+            return JsonResponse({'status': 'error', 'errors': form.errors})
     else:
         form = BookForm()
-    return render(request, 'publisher/create_book.html', {'form': form})
+    return render(request, 'create_book.html', {'form': form})
 
 def edit_book(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
@@ -18,12 +21,12 @@ def edit_book(request, book_id):
         form = BookForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
-            return redirect('publisher:book_list')
+            return redirect('Home')
     else:
         form = BookForm(instance=book)
-    return render(request, 'publisher/create_book.html', {'form': form})
+    return render(request, 'create_book.html', {'form': form})
 
 def delete_book(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     book.delete()
-    return redirect('publisher:book_list')
+    return redirect('Home')
