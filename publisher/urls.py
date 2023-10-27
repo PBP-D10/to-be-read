b2b9106a1476b67e8b5182855e8f67d741fa14c8
Joseph BIntang <joseph.bintang@ui.ac.id>
2023-10-27 21:26:49 +0700

    create book

--- publisher/urls.py
+++ publisher/urls.py
@@ -5,7 +5,6 @@ app_name = 'publisher'
 
 urlpatterns = [
     path('create/', views.create_book, name='create_book'),
-    path('edit/<int:book_id>/', views.edit_book, name='edit_book'),
     path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
     # Add other urls as required
 ]
--- publisher/views.py
+++ publisher/views.py
@@ -15,17 +15,6 @@ def create_book(request):
         form = BookForm()
     return render(request, 'create_book.html', {'form': form})
 
-def edit_book(request, book_id):
-    book = get_object_or_404(Book, pk=book_id)
-    if request.method == "POST":
-        form = BookForm(request.POST, instance=book)
-        if form.is_valid():
-            form.save()
-            return redirect('Home')
-    else:
-        form = BookForm(instance=book)
-    return render(request, 'create_book.html', {'form': form})
-
 def delete_book(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     book.delete()
