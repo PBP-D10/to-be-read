08f5b924c3fb89a8a823b5c64b6f819aa4a0dee3
Maurice Yang <mauriceyangmy28@gmail.com>
2023-10-27 17:24:07 +0700

    feat: Adding to urlpatterns

--- book/urls.py
+++ book/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # path('', get_books, name='get_books'),
     path('', views.home_page, name='Home'),
     path('books/<int:book_id>/', views.book_detail, name='Book Detail'),
+    path('api/books/', views.get_all_books, name='Get All Books'),
 ]
