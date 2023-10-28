71bd074dc561e092520dd0b22fcd0285186ea673
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2023-10-28 17:51:12 +0700

    order by

--- reader/urls.py
+++ reader/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+#from book import Book
 
 app_name = 'reader'
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('create_ajax', views.create_ajax, name='create_ajax'),
     path('profile/edit', views.edit_profile, name='edit_profile'),
     path('profile', views.view_profile, name='view_profile'),
+    path('mytbr/<int:book_id>/', views.saved_detail, name='saved_detail'),
 ]
