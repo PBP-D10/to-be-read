45d8f92e23a96348547cbc49e8d31c3b39c3d6a8
sekargandesdianti <sekargandesdianti@gmail.com>
2023-10-28 14:22:17 +0700

    Added HTML template for displaying Publisher Houses

--- publisher/models.py
+++ publisher/models.py
@@ -20,6 +20,12 @@ class Publisher(models.Model):
     name = models.CharField(max_length=50, null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 class PublisherHouse(models.Model):
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
     year_established = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.publisher.name
