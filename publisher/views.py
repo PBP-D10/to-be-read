47c121999f8652690931b121a7130b0ed29ecb5e
sekargandesdianti <sekargandesdianti@gmail.com>
2023-10-28 14:14:09 +0700

    Added views for retrieving publisher houses and rendering the publisher house page

--- publisher/views.py
+++ publisher/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import *
+from django.http import *
+from .models import Publisher, PublisherHouse, Book
+from .forms import BookForm
+from django.core import serializers
 
-# Create your views here.
+def get_publisher_house(request):
+    publisher_houses = PublisherHouse.objects.all()
+    data = [{'name': house.publisher.name, 'year_established': house.year_established} for house in publisher_houses]
+    return JsonResponse(data, safe=False)
+
+def houses(request):
+    return render(request, 'publisher_house_page.html')
