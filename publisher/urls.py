6a2498bf14785280e9e66808f4def3c22672c2f8
sekargandesdianti <sekargandesdianti@gmail.com>
2023-10-28 17:36:45 +0700

    Added URL pattern for 'edit_book' views

--- publisher/urls.py
+++ publisher/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import get_publisher_house, houses
+from .views import get_publisher_house, houses, edit_book
+
+app_name = 'publisher'
 
 urlpatterns = [
+    path('get-houses/', get_publisher_house, name='get_publisher_house'),
     path('houses/', houses, name='houses'),
-    path('get-houses/', get_publisher_house, name='get_publisher_houses')
+    path('edit-book/<int:book_id>/', edit_book, name='edit_book'),
 ]
