9cfd6ce722ffacb46cdfa02eb54a13056e801422
sekargandesdianti <sekargandesdianti@gmail.com>
2023-10-28 17:38:11 +0700

    Added view function for editing a book

--- publisher/views.py
+++ publisher/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import *
 from django.http import *
-from .models import Publisher, PublisherHouse, Book
+from .models import PublisherHouse
+from book.models import Book
 from .forms import BookForm
-from django.core import serializers
+from .decorators import publisher_required
 
 def get_publisher_house(request):
     publisher_houses = PublisherHouse.objects.all()
@@ -11,3 +12,19 @@ def get_publisher_house(request):
 
 def houses(request):
     return render(request, 'publisher_house_page.html')
+
+@publisher_required
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book:Home')
+        else:
+            return JsonResponse({'status': 'error', 'errors': form.errors})
+    else:
+        form = BookForm(instance=book)
+    
+    return render(request, 'edit_book.html', {'form': form, 'book': book})
