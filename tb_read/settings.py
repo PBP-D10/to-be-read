66f37c9f1fa4a70f9097ff775604be9ce9275e9e
Maurice Yang <mauriceyangmy28@gmail.com>
2023-10-29 21:34:11 +0700

    fix: Trying to fix CSRF error

--- tb_read/settings.py
+++ tb_read/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-4silik)13u%e^p=_7&qsnzxq6bqd8mk86+&-m*sk2p^m)s0rk2
 # SECURITY WARNING: don't run with debug turned on in production!
 PRODUCTION = env.bool('PRODUCTION', False)
 
-CSRF_TRUSTED_ORIGINS = ["*"]  
+CSRF_TRUSTED_ORIGINS = ["https://*.railway.app"]  
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
