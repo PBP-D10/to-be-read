7f2328a883654c7a6b410a94cc2c64ee91429cf1
Maurice Yang <mauriceyangmy28@gmail.com>
2023-12-18 10:33:52 +0700

    fix: Fixing some indentation bs

--- reader/views.py
+++ reader/views.py
@@ -13,9 +13,11 @@ from django.core import serializers
 import json
 
 # Create your views here.
+
+
 @login_required(login_url='login')
 @csrf_exempt
-def show_saved(request):       
+def show_saved(request):
     saved_books = SavedBook.objects.filter(owner=request.user)
 
     context = {
@@ -23,12 +25,14 @@ def show_saved(request):
     }
     return render(request, "my_tbr.html", context)
 
+
 @csrf_exempt
 def create_ajax(request):
     if request.method == 'POST':
         book_id = request.POST.get("book")
 
-        new_book = SavedBook.objects.get_or_create(owner=request.user, book_id=book_id)
+        new_book = SavedBook.objects.get_or_create(
+            owner=request.user, book_id=book_id)
         if (new_book[1] == False):
             return HttpResponse(b"ALREADY_EXISTS", status=200)
         else:
@@ -37,10 +41,12 @@ def create_ajax(request):
 
     return HttpResponseNotFound()
 
+
 def view_profile(request):
     current_profile = Profile.objects.get(user=request.user)
-    context = {'profile':current_profile}
+    context = {'profile': current_profile}
     return render(request, "profile.html", context)
+
 
 def get_profile_json(request):
     profile = Profile.objects.get(user=request.user)
@@ -52,10 +58,12 @@ def get_profile_json(request):
     }
     return JsonResponse(profile_data)
 
+
 @csrf_exempt
 def get_savedBook_json(request):
     savedBook = SavedBook.objects.filter(owner=request.user)
     return HttpResponse(serializers.serialize('json', savedBook), content_type="application/json")
+
 
 @csrf_exempt
 def edit_profile_ajax(request):
@@ -71,7 +79,7 @@ def edit_profile_ajax(request):
             profile.name = name
         if email != profile.email:
             profile.email = email
-            
+
         if address == '' or address == '-':
             profile.address = None
         else:
@@ -90,11 +98,12 @@ def edit_profile_ajax(request):
 
 
 def saved_detail(request, book_id):
-    book = Book.objects.get(id =book_id)
+    book = Book.objects.get(id=book_id)
     context = {
         'book': book,
     }
     return render(request, 'saved_detail.html', context=context)
+
 
 @csrf_exempt
 def create_quote(request):
@@ -107,18 +116,20 @@ def create_quote(request):
 
     return HttpResponseNotFound()
 
+
 def get_quote_json(request):
     quote = Quote.objects.all()
     return HttpResponse(serializers.serialize('json', quote), content_type="application/json")
 
+
 @csrf_exempt
 def create_quote_flutter(request):
     if request.method == 'POST':
-        
+
         data = json.loads(request.body)
 
         new_quote = Quote.objects.create(
-            text = data["text"],
+            text=data["text"],
         )
 
         new_quote.save()
@@ -127,21 +138,23 @@ def create_quote_flutter(request):
     else:
         return JsonResponse({"status": "error"}, status=401)
 
+
 def show_json_by_id(request, id):
     data = Book.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+
 @csrf_exempt
 def create_saved_flutter(request):
     if request.method == 'POST':
-        
+
         data = json.loads(request.body)
 
         # new_book = SavedBook.objects.get_or_create(owner=request.user, book_id=book_id)
 
         new_saved = SavedBook.objects.get_or_create(
-            owner = request.user,
-            book_id = int(data["book"]["pk"]),
+            owner=request.user,
+            book_id=int(data["book"]["pk"]),
         )
 
         if (new_saved[1] == True):
@@ -155,40 +168,43 @@ def create_saved_flutter(request):
         #     description = data["description"]
         # )
 
-        #new_saved.save()
+        # new_saved.save()
 
         return JsonResponse({"status": "success"}, status=200)
     else:
         return JsonResponse({"status": "error"}, status=401)
     return HttpResponse(serializers.serialize('json', quote))
 
+
 @csrf_exempt
 def like_book_ajax(request):
     if request.method == 'POST':
 
         data = json.loads(request.body)
-        book_id = data.get("book","")
+        book_id = data.get("book", "")
 
         print('user: ', request.user)
         print('book_id: ', book_id)
 
-        new_book = LikedBook.objects.get_or_create(owner=request.user, book_id=book_id)
-        
+        new_book = LikedBook.objects.get_or_create(
+            owner=request.user, book_id=book_id)
+
         if (new_book[1] == False):
             return JsonResponse({"status": "ALREADY_EXISTS"}, status=200)
         else:
             new_book[0].save()
-            
+
         return JsonResponse({"status": "created"}, status=200)
-     return JsonResponse({"status": "not a post"}, status=200)
+    return JsonResponse({"status": "not a post"}, status=200)
+
 
 @csrf_exempt
 def edit_profile_flutter(request):
     profile = Profile.objects.get(user=request.user)
-    
+
     if request.method == 'POST':
         data = json.loads(request.body)
-        
+
         name = data['name']
         email = data['email']
         address = data['address']
@@ -198,7 +214,7 @@ def edit_profile_flutter(request):
             profile.name = name
         if email != profile.email:
             profile.email = email
-            
+
         if address == '' or address == '-':
             profile.address = None
         else:
@@ -215,13 +231,16 @@ def edit_profile_flutter(request):
 
     return JsonResponse({"status": "error"}, status=401)
 
+
 def get_profile_json_flutter(request):
     profile = Profile.objects.get(user=request.user)
     return HttpResponse(serializers.serialize("json", [profile]), content_type="application/json")
 
+
 def get_saved_books_json_flutter(request):
     savedBooks = SavedBook.objects.filter(owner=request.user)
     return HttpResponse(serializers.serialize("json", savedBooks), content_type="application/json")
+
 
 def get_all_profile_json(request):
     profile = Profile.objects.all()
